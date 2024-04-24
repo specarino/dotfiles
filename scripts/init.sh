e17eebbf171f8bcf63951d65a5d161b3574c9fa8
specarino <me@specarino.com>
2024-04-24 12:25:23 +0100

    add init script

---
+++ scripts/init.sh
@@ -0,0 +1,10 @@
+git remote add --fetch --track master oh-my-zsh https://github.com/ohmyzsh/ohmyzsh.git
+git remote add --fetch --track master powerlevel10k https://github.com/romkatv/powerlevel10k.git
+git remote add --fetch --track master zsh-completions https://github.com/zsh-users/zsh-completions.git
+git remote add --fetch --track master zsh-autosuggestions https://github.com/zsh-users/zsh-autosuggestions.git
+git remote add --fetch --track master zsh-syntax-highlighting https://github.com/zsh-users/zsh-syntax-highlighting.git
+git remote add --fetch --track master zsh-history-substring-search https://github.com/zsh-users/zsh-history-substring-search.git
+git remote add --fetch --track main NvChad https://github.com/NvChad/starter.git
+git remote add --fetch --track master pyenv https://github.com/pyenv/pyenv.git
+git remote add --fetch --track master pyenv-virtualenv https://github.com/pyenv/pyenv-virtualenv.git
+git remote add --fetch --track master tpm https://github.com/tmux-plugins/tpm.git
