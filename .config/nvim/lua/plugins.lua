ba92b5e659da17c192864c838a5760eef50934f6
specarino <me@specarino.com>
2024-05-07 08:06:54 +0100

    nvim uses catppuccin now instead of tokyonight

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -2,143 +2,143 @@
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Lazy Plugins
 
 require("lazy").setup({
-    {
-        "folke/tokyonight.nvim",
-        config = function()
-            vim.cmd.colorscheme("tokyonight-night")
-        end,
-    },
+	{
+		"catppuccin/nvim",
+		config = function()
+			vim.cmd.colorscheme("catppuccin-mocha")
+		end,
+	},
 
-    {
-        "nvim-treesitter/nvim-treesitter",
-        config = function()
-            require("nvim-treesitter.configs").setup({
-                ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python" },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python" },
 
-                auto_install = true,
+				auto_install = true,
 
-                highlight = {
-                    enable = true,
-                },
+				highlight = {
+					enable = true,
+				},
 
-                indent = {
-                    enable = true,
-                },
+				indent = {
+					enable = true,
+				},
 
-                incremental_selection = {
-                    enable = true,
-                    keymaps = {
-                        init_selection = "<Leader>ss",
-                        node_incremental = "<Leader>si",
-                        scope_incremental = "<Leader>sc",
-                        node_decremental = "<Leader>sd",
-                    },
-                },
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = "<Leader>ss",
+						node_incremental = "<Leader>si",
+						scope_incremental = "<Leader>sc",
+						node_decremental = "<Leader>sd",
+					},
+				},
 
-                textobjects = {
-                    select = {
-                        enable = true,
+				textobjects = {
+					select = {
+						enable = true,
 
-                        -- Automatically jump forward to textobj, similar to targets.vim
-                        lookahead = true,
+						-- Automatically jump forward to textobj, similar to targets.vim
+						lookahead = true,
 
-                        keymaps = {
-                            -- You can use the capture groups defined in textobjects.scm
-                            ["af"] = "@function.outer",
-                            ["if"] = "@function.inner",
-                            ["ac"] = "@class.outer",
-                            -- You can optionally set descriptions to the mappings (used in the desc parameter of
-                            -- nvim_buf_set_keymap) which plugins like which-key display
-                            ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-                            -- You can also use captures from other query groups like `locals.scm`
-                            ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
-                        },
-                        -- You can choose the select mode (default is charwise 'v')
-                        --
-                        -- Can also be a function which gets passed a table with the keys
-                        -- * query_string: eg '@function.inner'
-                        -- * method: eg 'v' or 'o'
-                        -- and should return the mode ('v', 'V', or '<c-v>') or a table
-                        -- mapping query_strings to modes.
-                        selection_modes = {
-                            ['@parameter.outer'] = 'v', -- charwise
-                            ['@function.outer'] = 'V', -- linewise
-                            ['@class.outer'] = '<c-v>', -- blockwise
-                        },
-                        -- If you set this to `true` (default is `false`) then any textobject is
-                        -- extended to include preceding or succeeding whitespace. Succeeding
-                        -- whitespace has priority in order to act similarly to eg the built-in
-                        -- `ap`.
-                        --
-                        -- Can also be a function which gets passed a table with the keys
-                        -- * query_string: eg '@function.inner'
-                        -- * selection_mode: eg 'v'
-                        -- and should return true or false
-                        include_surrounding_whitespace = true,
-                    },
-                },
-            })
-        end,
-    },
+						keymaps = {
+							-- You can use the capture groups defined in textobjects.scm
+							["af"] = "@function.outer",
+							["if"] = "@function.inner",
+							["ac"] = "@class.outer",
+							-- You can optionally set descriptions to the mappings (used in the desc parameter of
+							-- nvim_buf_set_keymap) which plugins like which-key display
+							["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+							-- You can also use captures from other query groups like `locals.scm`
+							["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+						},
+						-- You can choose the select mode (default is charwise 'v')
+						--
+						-- Can also be a function which gets passed a table with the keys
+						-- * query_string: eg '@function.inner'
+						-- * method: eg 'v' or 'o'
+						-- and should return the mode ('v', 'V', or '<c-v>') or a table
+						-- mapping query_strings to modes.
+						selection_modes = {
+							["@parameter.outer"] = "v", -- charwise
+							["@function.outer"] = "V", -- linewise
+							["@class.outer"] = "<c-v>", -- blockwise
+						},
+						-- If you set this to `true` (default is `false`) then any textobject is
+						-- extended to include preceding or succeeding whitespace. Succeeding
+						-- whitespace has priority in order to act similarly to eg the built-in
+						-- `ap`.
+						--
+						-- Can also be a function which gets passed a table with the keys
+						-- * query_string: eg '@function.inner'
+						-- * selection_mode: eg 'v'
+						-- and should return true or false
+						include_surrounding_whitespace = true,
+					},
+				},
+			})
+		end,
+	},
 
-    {
-        "nvim-treesitter/nvim-treesitter-textobjects",
-    },
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+	},
 
-    {
-        "neovim/nvim-lspconfig",
-    },
+	{
+		"neovim/nvim-lspconfig",
+	},
 
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end,
-    },
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
 
-    {
-        "williamboman/mason-lspconfig.nvim",
-        dependencies = { "mason.nvim" },
-        config = function()
-            require("mason-lspconfig").setup()
-            require("mason-lspconfig").setup_handlers({
-                function(server_name)
-                    require("lspconfig")[server_name].setup({})
-                end,
-            })
-        end,
-    },
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = { "mason.nvim" },
+		config = function()
+			require("mason-lspconfig").setup()
+			require("mason-lspconfig").setup_handlers({
+				function(server_name)
+					require("lspconfig")[server_name].setup({})
+				end,
+			})
+		end,
+	},
 
-    -- Bufferline
-    {
-        'akinsho/bufferline.nvim',
-        dependencies = {
-            'nvim-tree/nvim-web-devicons'
-        },
-        config = function()
-            require("bufferline").setup({
-                options = {
-                    hover = {
-                        enabled = true,
-                        delay = 150,
-                        reveal = {'close'}
-                    }
-                }
-            })
-        end,
-    },
+	-- Bufferline
+	{
+		"akinsho/bufferline.nvim",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("bufferline").setup({
+				options = {
+					hover = {
+						enabled = true,
+						delay = 150,
+						reveal = { "close" },
+					},
+				},
+			})
+		end,
+	},
 })
